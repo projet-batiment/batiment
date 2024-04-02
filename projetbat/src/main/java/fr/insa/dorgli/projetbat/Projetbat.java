51f3aef925f3831e7c2b0543819d5f61f659a16d
nonolacigogne <162794611+nonolacigogne@users.noreply.github.com>
2024-04-02 11:10:02 +0200

    Ajout des methodes getById

--- projetbat/src/main/java/fr/insa/dorgli/projetbat/Projetbat.java
+++ projetbat/src/main/java/fr/insa/dorgli/projetbat/Projetbat.java
@@ -21,6 +21,64 @@ public class Projetbat {
     HashMap<Integer, TypeRevetement> typesRevetement = new HashMap<Integer, TypesRevetement>();
 
     HashMap<Integer, Mur> murs= new HashMap<>();
+
+public Mur getMurById(int id){
+	return murs.get(id);
+}
+
+public Piece getPieceById(int id){
+	return pieces.get(id);
+}
+
+public Appart getAppartById(int id){
+	return apparts.get(id);
+}
+
+public Niveau getNiveauById(int id){
+	return niveaux.get(id);
+}
+
+public OuvertureMur getOuvertureMurById(int id){
+	return ouverturesMur.get(id);
+}
+
+public OuvertureNiveau getOuvertureNiveauById(int id){
+	return ouverturesNiveaux.get(id);
+}
+
+public RevetementMur getRevetementMurById(int id){
+	return revetementsMur.get(id);
+}
+
+public PlafondSol getSolPlafondById(int id){
+	return plafondsSols.get(id);
+}
+
+public TypeMur getTypeMurById(int id){
+	return typesMur.get(id);
+}
+
+public Point getPointById(int id){
+	return points.get(id);
+}
+
+public TypeOuvertureNiveau getTypeOuvertureNiveauById(int id){
+	return typesOuverturesNiveau.get(id);
+}
+
+public TypeOuvertureMur getTypeOuvertureMurById(int id){
+	return typesOuverturesMur.get(id);
+}
+
+public TypeRevetement getTypeRevetementById(int id){
+	return typesRevetement.get(id);
+}
+
+public RevetementPlafondSol getRevetementPlafondSolById(int id){
+	return revetementsPlafondSol.get(id);
+}
+
+    
     public static void main(String[] args) {
         Deserialize.main(args); // pour l'instant
 
