e2c5652b84b6100f35efa539001e0de780dba4ba
lapingenieur <laipngenieur@gmail.com>
2024-04-02 11:01:24 +0200

    corrections des hashmap

--- projetbat/src/main/java/fr/insa/dorgli/projetbat/Projetbat.java
+++ projetbat/src/main/java/fr/insa/dorgli/projetbat/Projetbat.java
@@ -3,26 +3,26 @@ package fr.insa.dorgli.projetbat;
 import java.util.HashMap;
 
 public class Projetbat {
+    HashMap<Integer, Niveau> niveaux = new HashMap<Integer, Niveau>();
+    HashMap<Integer, Appart> apparts = new HashMap<Integer, Appart>();
+    HashMap<Integer, Piece> pieces = new HashMap<Integer, Piece>();
+    HashMap<Integer, PlafondSol> plafondsSols = new HashMap<Integer, PlafondSol>();
+    HashMap<Integer, Point> points = new HashMap<Integer, Point>();
+
+    HashMap<Integer, OuvertureMur> ouverturesMur = new HashMap<Integer, OuvertureMur>();
+    HashMap<Integer, OuvertureNiveaux> ouverturesNiveaux = new HashMap<Integer, OuvertureNiveaux>();
+
+    HashMap<Integer, RevetementMur> revetementsMur = new HashMap<Integer, RevetementMur>();
+    HashMap<Integer, RevetementPlafondSol> revetementsPlafondSol = new HashMap<Integer, RevetementPlafondSol>();
+
+    HashMap<Integer, TypeMur> typesMur = new HashMap<Integer, TypeMur>();
+    HashMap<Integer, TypeOuvertureMur> typesOuverturesMur = new HashMap<Integer, TypesOuverturesMur>();
+    HashMap<Integer, TypeOuvertureNiveau> typesOuverturesNiveau = new HashMap<Integer, TypesOuverturesNiveau>();
+    HashMap<Integer, TypeRevetement> typesRevetement = new HashMap<Integer, TypesRevetement>();
+
     public static void main(String[] args) {
         Deserialize.main(args); // pour l'instant
 
         Batiment batiment = new Batiment();
-
-        HashMap<Integer, Niveau> niveaux = new HashMap<Integer, Niveau>();
-        HashMap<Integer, Appart> apparts = new HashMap<Integer, Appart>();
-        HashMap<Integer, Piece> pieces = new HashMap<Integer, Piece>();
-        HashMap<Integer, PlafondSol> plafondsSols = new HashMap<Integer, PlafondSol>();
-        HashMap<Integer, Point> points = new HashMap<Integer, Point>();
-
-        HashMap<Integer, OuvertureMur> ouverturesMur = new HashMap<Integer, OuvertureMur>();
-        HashMap<Integer, OuvertureNiveaux> ouverturesNiveaux = new HashMap<Integer, OuvertureNiveaux>();
-
-        HashMap<Integer, RevetementMur> revetementsMur = new HashMap<Integer, RevetementMur>();
-        HashMap<Integer, RevetementPlafondSol> revetementsPlafondSol = new HashMap<Integer, RevetementPlafondSol>();
-
-        HashMap<Integer, TypeMur> typeMur = new HashMap<Integer, TypeMur>();
-        HashMap<Integer, TypeOuvertureMur> typesOuverturesMur = new HashMap<Integer, TypesOuverturesMur>();
-        HashMap<Integer, TypeOuvertureNiveau> typesOuverturesNiveau = new HashMap<Integer, TypesOuverturesNiveau>();
-        HashMap<Integer, TypeRevetement> typesRevetement = new HashMap<Integer, TypesRevetement>();
     }
 }
