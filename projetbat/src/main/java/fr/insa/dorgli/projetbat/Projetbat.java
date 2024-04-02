6a98770e92b89ea85a3350818dfd9d8b1b2d572b
lapingenieur <laipngenieur@gmail.com>
2024-04-02 11:35:38 +0200

    formattage

--- projetbat/src/main/java/fr/insa/dorgli/projetbat/Projetbat.java
+++ projetbat/src/main/java/fr/insa/dorgli/projetbat/Projetbat.java
@@ -6,6 +6,7 @@ public class Projetbat {
     HashMap<Integer, Niveau> niveaux = new HashMap<Integer, Niveau>();
     HashMap<Integer, Appart> apparts = new HashMap<Integer, Appart>();
     HashMap<Integer, Piece> pieces = new HashMap<Integer, Piece>();
+    HashMap<Integer, Mur> murs = new HashMap<>();
     HashMap<Integer, PlafondSol> plafondsSols = new HashMap<Integer, PlafondSol>();
     HashMap<Integer, Point> points = new HashMap<Integer, Point>();
 
@@ -20,65 +21,62 @@ public class Projetbat {
     HashMap<Integer, TypeOuvertureNiveau> typesOuverturesNiveau = new HashMap<Integer, TypesOuverturesNiveau>();
     HashMap<Integer, TypeRevetement> typesRevetement = new HashMap<Integer, TypesRevetement>();
 
-    HashMap<Integer, Mur> murs= new HashMap<>();
+    public Mur getMurById(int id) {
+        return murs.get(id);
+    }
 
-public Mur getMurById(int id){
-	return murs.get(id);
-}
+    public Piece getPieceById(int id) {
+        return pieces.get(id);
+    }
 
-public Piece getPieceById(int id){
-	return pieces.get(id);
-}
+    public Appart getAppartById(int id) {
+        return apparts.get(id);
+    }
 
-public Appart getAppartById(int id){
-	return apparts.get(id);
-}
+    public Niveau getNiveauById(int id) {
+        return niveaux.get(id);
+    }
 
-public Niveau getNiveauById(int id){
-	return niveaux.get(id);
-}
+    public OuvertureMur getOuvertureMurById(int id) {
+        return ouverturesMur.get(id);
+    }
 
-public OuvertureMur getOuvertureMurById(int id){
-	return ouverturesMur.get(id);
-}
+    public OuvertureNiveau getOuvertureNiveauById(int id) {
+        return ouverturesNiveaux.get(id);
+    }
 
-public OuvertureNiveau getOuvertureNiveauById(int id){
-	return ouverturesNiveaux.get(id);
-}
+    public RevetementMur getRevetementMurById(int id) {
+        return revetementsMur.get(id);
+    }
 
-public RevetementMur getRevetementMurById(int id){
-	return revetementsMur.get(id);
-}
+    public PlafondSol getSolPlafondById(int id) {
+        return plafondsSols.get(id);
+    }
 
-public PlafondSol getSolPlafondById(int id){
-	return plafondsSols.get(id);
-}
+    public TypeMur getTypeMurById(int id) {
+        return typesMur.get(id);
+    }
 
-public TypeMur getTypeMurById(int id){
-	return typesMur.get(id);
-}
+    public Point getPointById(int id) {
+        return points.get(id);
+    }
 
-public Point getPointById(int id){
-	return points.get(id);
-}
+    public TypeOuvertureNiveau getTypeOuvertureNiveauById(int id) {
+        return typesOuverturesNiveau.get(id);
+    }
 
-public TypeOuvertureNiveau getTypeOuvertureNiveauById(int id){
-	return typesOuverturesNiveau.get(id);
-}
+    public TypeOuvertureMur getTypeOuvertureMurById(int id) {
+        return typesOuverturesMur.get(id);
+    }
 
-public TypeOuvertureMur getTypeOuvertureMurById(int id){
-	return typesOuverturesMur.get(id);
-}
+    public TypeRevetement getTypeRevetementById(int id) {
+        return typesRevetement.get(id);
+    }
 
-public TypeRevetement getTypeRevetementById(int id){
-	return typesRevetement.get(id);
-}
+    public RevetementPlafondSol getRevetementPlafondSolById(int id) {
+        return revetementsPlafondSol.get(id);
+    }
 
-public RevetementPlafondSol getRevetementPlafondSolById(int id){
-	return revetementsPlafondSol.get(id);
-}
-
-    
     public static void main(String[] args) {
         Deserialize.main(args); // pour l'instant
 
