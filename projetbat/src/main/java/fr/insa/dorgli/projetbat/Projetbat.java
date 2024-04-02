09e86095175bb5ab540153be71c2ff5121de3dea
rdorgli01 <rdorgli01@SEC-C210-F15.csip.in>
2024-04-02 11:00:56 +0200

    ajout HashMap Mur

--- projetbat/src/main/java/fr/insa/dorgli/projetbat/Projetbat.java
+++ projetbat/src/main/java/fr/insa/dorgli/projetbat/Projetbat.java
@@ -1,6 +1,7 @@
 package fr.insa.dorgli.projetbat;
 
 public class Projetbat {
+    HashMap<Integer, Mur> murs= new HashMap<>();
     public static void main(String[] args) {
         Deserialize.main(args); // pour l'instant
     }
