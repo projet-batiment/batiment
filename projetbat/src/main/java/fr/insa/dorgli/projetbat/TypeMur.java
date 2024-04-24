c10de52e210fc86006b6a8707d857f0dafc0e304
nonolacigogne <162794611+nonolacigogne@users.noreply.github.com>
2024-04-24 16:54:11 +0200

    Update PrixStructureMur.java

--- projetbat/src/main/java/fr/insa/dorgli/projetbat/TypeMur.java
+++ projetbat/src/main/java/fr/insa/dorgli/projetbat/TypeMur.java
@@ -45,6 +45,11 @@ public class TypeMur implements ToStringShort {
 		this.prixUnitaire = prixUnitaire;
 	}
 
+	public double PrixStructureMur(){
+		prixStructureMur = (aireMur * this.prixUnitaire);
+		return prixStructureMur;
+	}
+
 	public String toString() {
 		return "TypeMur { epaisseur: " + epaisseur + ", prixUnitaire: " + prixUnitaire + ", nom: " + nom + " }";
 	}
